daf545f13e12d5e9dc9e28d829013ceaf97ccbaa
Tungaptech <nghiemthanhtung2013@gmail.com>
2024-06-12 21:24:54 +0700

    Create ArticleRepository.java

---
+++ Java/AS02/assignment2/ArticleRepository.java
@@ -0,0 +1,12 @@
+package assignment2;
+
+import java.util.ArrayList;
+
+public interface ArticleRepository {
+    ArrayList<Article> findAll();
+    Article findByUrl(String url);
+    Article save(Article article);
+    Article update(Article article);
+    void deleteByUrl(String url);
+
+}
