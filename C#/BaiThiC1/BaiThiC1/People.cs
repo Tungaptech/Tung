5bbd4418d392d61930da0689b11dc345f0f652de
Tungaptech <nghiemthanhtung2013@gmail.com>
2024-07-24 19:39:35 +0700

    Update People.cs

--- C#/BaiThiC1/BaiThiC1/People.cs
+++ C#/BaiThiC1/BaiThiC1/People.cs
@@ -2,52 +2,9 @@
 
 using System;
 
-class People
+public class People
 {
-    // Các trường riêng để lưu trữ các thuộc tính
-    private string name;
-    private bool gender;
-    private int age;
-
-    // Các thuộc tính
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }
-    }
-
-    public bool Gender
-    {
-        get { return gender; }
-        set { gender = value; }
-    }
-
-    public int Age
-    {
-        get { return age; }
-        set { age = value; }
-    }
-
-    // Constructor
-    public People(string name, bool gender, int age)
-    {
-        Name = name;
-        Gender = gender;
-        Age = age;
-    }
-
-    // Phương thức để hiển thị các giá trị hiện tại của các thuộc tính
-    public void Display()
-    {
-        Console.WriteLine($"Tên: {Name}");
-        Console.WriteLine($"Giới tính: {(Gender ? "Nam" : "Nữ")}");
-        Console.WriteLine($"Tuổi: {Age}");
-    }
-
-    // Phương thức để tăng giá trị của thuộc tính Age
-    public void IncrementAge()
-    {
-        Age++;
-    }
+    public string Name { get; set; }
+    public bool Gender { get; set; } // true for male, false for female
+    public int Age { get; set; }
 }
-
