3c50620f47bc1f3acf2c37f158b6f2bcda315241
Tungaptech <nghiemthanhtung2013@gmail.com>
2024-07-24 19:39:27 +0700

    Update Program.cs

--- C#/BaiThiC1/BaiThiC1/Program.cs
+++ C#/BaiThiC1/BaiThiC1/Program.cs
@@ -4,22 +4,12 @@ class Program
 {
     static void Main()
     {
-        // Tạo một đối tượng của lớp People
-        People person = new People("Marry", false, 35);
+        
+        PeopleDemo peopleDemo = new PeopleDemo();
+        peopleDemo.DemonstrateProperties();
 
-        // Hiển thị các giá trị ban đầu
-        Console.WriteLine("Các giá trị ban đầu:");
-        person.Display();
-        Console.WriteLine();
-
-        // Tăng tuổi
-        person.IncrementAge();
-
-        // Hiển thị các giá trị sau khi tăng tuổi
-        Console.WriteLine("Sau khi tăng tuổi:");
-        person.Display();
-
-        Console.ReadLine(); // Để giữ cửa sổ console mở
+        
+        WordSearch wordSearch = new WordSearch();
+        wordSearch.SearchWordInString();
     }
-    
 }
