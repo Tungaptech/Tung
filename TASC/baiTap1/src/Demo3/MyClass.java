e84cb58f33ce07f3094071f8c00bb125af19c969
Tungaptech <nghiemthanhtung2013@gmail.com>
2024-10-16 15:29:51 +0700

    Create MyClass.java

---
+++ TASC/baiTap1/src/Demo3/MyClass.java
@@ -0,0 +1,11 @@
+package Demo3;
+
+public class MyClass {
+    static int staticVariable = 0; // thuộc tính static
+
+    static void staticMethod() {    // phương thức static
+        System.out.println("This is a static method.");
+    }
+}
+
+
