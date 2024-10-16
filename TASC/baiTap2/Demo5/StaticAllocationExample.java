2568d8faff9427c2526a1a820c962a59184f3f3f
Tungaptech <nghiemthanhtung2013@gmail.com>
2024-10-16 17:21:33 +0700

    Create StaticAllocationExample.java

---
+++ TASC/baiTap2/Demo5/StaticAllocationExample.java
@@ -0,0 +1,9 @@
+package Demo5;
+
+public class StaticAllocationExample {
+    static int staticVariable = 10; // Cấp phát tĩnh
+
+    public static void main(String[] args) {
+        System.out.println("Static Variable: " + staticVariable);
+    }
+}
