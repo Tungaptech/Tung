6193022530c10c9bf5ac38c6984d1080e7e88d84
Tungaptech <nghiemthanhtung2013@gmail.com>
2024-10-16 15:29:48 +0700

    Create Main.java

---
+++ TASC/baiTap1/src/Demo3/Main.java
@@ -0,0 +1,9 @@
+package Demo3;
+
+// Truy cập static
+public class Main {
+    public static void main(String[] args) {
+        MyClass.staticVariable = 10; // truy cập thuộc tính static
+        MyClass.staticMethod();       // truy cập phương thức static
+    }
+}
