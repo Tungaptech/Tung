8ddbc36ddc2712cbfa81eff61f76996cd884da88
Tungaptech <nghiemthanhtung2013@gmail.com>
2024-10-17 11:57:48 +0700

    Create ThreadExample.java

---
+++ TASC/Thread/ThreadExample.java
@@ -0,0 +1,8 @@
+package Thread;
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start(); // Khởi động thread
+    }
+}
