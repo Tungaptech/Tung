ad1360492edc93b1221094a21c684514d38059e4
Tungaptech <nghiemthanhtung2013@gmail.com>
2024-10-17 10:08:36 +0700

    Create ArrayExample.java

---
+++ TASC/baitapArray/ArrayExample.java
@@ -0,0 +1,14 @@
+package baitapArray;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Khai báo và khởi tạo mảng
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Truy cập và in ra các phần tử
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Phần tử tại chỉ số " + i + ": " + numbers[i]);
+        }
+    }
+}
+
